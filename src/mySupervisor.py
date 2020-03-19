5b9daff68377875af902ea7a3a67333bd9a309b1
mSimon12 <marcelo_simon@hotmail.com>
2020-03-19 14:24:49 -0300

    basic functions

---
+++ src/mySupervisor.py
@@ -0,0 +1,19 @@
+
+from SCT import SupBuilder
+
+sup1 = SupBuilder()
+sup1.insert_state('A')
+sup1.insert_state('B')
+sup1.insert_state('C')
+sup1.insert_transition('t1','A','B')
+sup1.insert_transition('t2','C','B')
+sup1.insert_transition('t3','B','A')
+sup1.insert_transition('t3','C','B')
+sup1.show_states()
+
+sup1.remove_transition('t1','B')
+sup1.show_states()
+
+sup1.remove_state('A')
+sup1.show_states()
+
